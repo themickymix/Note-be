84bf5f65636f9c97c5eb47aa64ad44f8c56724e5
themickymix <micz.yalon@gmail.com>
2025-02-14 17:42:08 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = new Hono();
 app.use("*", cors({
-    origin: ["http://localhost:3001"],
+    origin: ["*"],
     allowHeaders: [
         "X-Custom-Header",
         "Upgrade-Insecure-Requests",
